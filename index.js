4e675e4409b9ef20d6f6b91f68de3d656193d58b
Assami Muzaki <muzakiassami@gmail.com>
2022-08-11 19:36:03 +0700

    cleaning

--- index.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios')
-
-async function reqData(){
-    const response = await axios.post('https://newsletter.pukulenam.id/campaigns-rss?a=gMUkVEEzvMQh1VqoNTyvyUBkHbxXA2&i=1')
-    console.log(response.data);
-}
-reqData()
--- public/parse.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById('myButton');
-button.addEventListener('click', function(e) {
-    console.log('button was clicked');
-
-  fetch('/clicked', {method: 'POST'})
-});
